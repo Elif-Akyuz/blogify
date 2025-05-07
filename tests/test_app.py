80c725134cdcf840f9a457e453dd797326350f11
Elif Akyuz <e.akyuz9498@gmail.com>
2025-05-07 20:41:20 +0300

    Test dosyasına yeni test ekledi

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,13 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import pytest
 from app import app
 
 def test_index():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
+    assert b"Hosgeldiniz" in response.data
+
