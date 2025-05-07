995acdd58b176cc4b410a7decd5b5bb80facda8c
Elif Akyuz <e.akyuz9498@gmail.com>
2025-05-07 22:23:15 +0300

    wsgi.py güncellendi

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,18 @@
+import sys
+import os
+
+# Path to your virtual environment
+virtualenv_path = '/home/elifakyuz/.virtualenvs/myenv'
+if virtualenv_path not in sys.path:
+    sys.path.insert(0, virtualenv_path)
+
+# Add your project directory to the sys.path
+project_home = '/home/elifakyuz/blogify'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# Activate the virtual environment
+activate_this = os.path.join(virtualenv_path, 'bin/activate_this.py')
+exec(open(activate_this).read(), dict(__file__=activate_this))
+
 from app import app as application
